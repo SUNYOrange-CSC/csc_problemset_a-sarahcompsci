8df527774a9a62d1219700fcaf416d4a646a2c2b
sarahcompsci <123202075+sarahcompsci@users.noreply.github.com>
2023-02-20 16:13:54 -0500

    Create chp2_question27.cpp

---
+++ chp2_question27.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int speed = 20, time = 10, distance; 
+
+  distance = speed * time;
+  cout << "The distance is " << distance << "." << endl;
+
+  return 0;
+}
